3edba26c48f919023d9741874bf5c6d4c11745c7
Igor <igor_socha@yahoo.com>
2023-11-08 14:37:44 +0100

    hw.js.07 v0.3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,24 +1,24 @@
-import { galleryItems } from './galleryItems.js';
-
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
 const gallery = document.querySelector('.gallery');
 
-function createGallery() {
-  galleryItems.forEach(item => {
-    const galleryItem = document.createElement('div');
-    galleryItem.classList.add('gallery-item');
+galleryItems.forEach(item => {
+  const galleryItem = document.createElement('div');
+  galleryItem.classList.add('gallery__item');
 
-    const image = document.createElement('img');
-    image.src = item.preview;
-    image.alt = item.description;
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.href = item.original;
 
-    galleryItem.appendChild(image);
-    gallery.appendChild(galleryItem);
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = item.preview;
+  image.setAttribute('data-source', item.original);
+  image.alt = item.description;
 
-    image.addEventListener('click', () => {
-      const lightbox = basicLightbox.create(`<img src="${item.original}" alt="${item.description}">`);
-      lightbox.show();
-    });
-  });
-}
+  link.appendChild(image);
+  galleryItem.appendChild(link);
+  gallery.appendChild(galleryItem);
+});
 
-createGallery();
+console.log(galleryItems);
