65254eea2786920587bfee9739c0154d503ca8d9
Igor <igor_socha@yahoo.com>
2023-11-09 10:22:52 +0100

    hw.js.07 v0.3.2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,25 +1,32 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-const gallery = document.querySelector('.gallery');
+import { galleryItems } from './gallery-items.js'; 
 
-galleryItems.forEach(item => {
-  const galleryItem = document.createElement('div');
-  galleryItem.classList.add('gallery__item');
+document.addEventListener('DOMContentLoaded', function() {
+  const gallery = document.querySelector('.gallery');
 
-  const link = document.createElement('a');
-  link.classList.add('gallery__link');
-  link.href = item.original;
-  link.setAttribute('target', '_blank');
+  galleryItems.forEach(item => {
+    const galleryItem = document.createElement('div');
+    galleryItem.classList.add('gallery__item');
 
-  const image = document.createElement('img');
-  image.classList.add('gallery__image');
-  image.src = item.preview;
-  image.setAttribute('data-source', item.original);
-  image.alt = item.description;
+    const link = document.createElement('a');
+    link.classList.add('gallery__link');
+    link.href = item.original;
+    link.setAttribute('target', '_blank');
 
-  link.appendChild(image);
-  galleryItem.appendChild(link);
-  gallery.appendChild(galleryItem);
+    const image = document.createElement('img');
+    image.classList.add('gallery__image');
+    image.src = item.preview;
+    image.setAttribute('data-source', item.original);
+    image.alt = item.description;
+
+    link.appendChild(image);
+    galleryItem.appendChild(link);
+    gallery.appendChild(galleryItem);
+
+    image.addEventListener('click', function() {
+      const instance = BasicLightbox.create(`<img src="${item.original}" alt="${item.description}">`);
+      instance.show();
+    });
+  });
+
+  console.log(galleryItems); 
 });
-
-console.log(galleryItems);
